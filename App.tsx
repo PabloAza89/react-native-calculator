4a8be8a7911d540e7c56c5b7893401a94f1f7762
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-04-16 00:35:59 -0300

    stop using react-native-CLI, start using Expo

--- App.tsx
+++ App.tsx
@@ -1,119 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  TouchableOpacity,
+  Pressable,
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
-  View,
+  View
 } from 'react-native';
+import { s } from './src/styles';
+import { OwnButton } from './src/components/OwnButton';
+/* import Icon from 'react-native-vector-icons/FontAwesome'; */
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={[s.background]}>
+      <View style={[s.contour]}>
+        {/* <View style={[s.upper]} /> */}
+        {/* <OwnButton value="M"></OwnButton> */}
+        <Ionicons name='md-checkmark-circle' size={32} color='green' />
+    
+        <OwnButton value="/"></OwnButton>
+        <OwnButton value="X"></OwnButton>
+        <OwnButton value=""></OwnButton>
+        <OwnButton value="7"></OwnButton>
+        <OwnButton value="8"></OwnButton>
+        <OwnButton value="9"></OwnButton>
+        <OwnButton value="/"></OwnButton>
+        <OwnButton value="4"></OwnButton>
+        <OwnButton value="5"></OwnButton>
+        <OwnButton value="6"></OwnButton>
+        <OwnButton value="-"></OwnButton>
+        <OwnButton value="1"></OwnButton>
+        <OwnButton value="2"></OwnButton>
+        <OwnButton value="3"></OwnButton>
+        <OwnButton value="+"></OwnButton>
+        <OwnButton value="M"></OwnButton>
+        <OwnButton value="0"></OwnButton>
+        <OwnButton value="."></OwnButton>
+        <OwnButton value="="></OwnButton>
+      </View>
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Test">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    backgroundColor: 'red',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
