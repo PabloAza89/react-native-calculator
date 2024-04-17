406af147096bc58b75f0881275dbfff3bdc94783
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-04-17 02:17:25 -0300

    added sum

--- src/components/test.js
+++ src/components/test.js
@@ -1,7 +1,9 @@
-let init = "12X2-10X2"
+let init = "123+100"
 let arr0 = init.split("") // ['S'P'L'I'T'E'D']
 let arr1 = [] // [10, X, 3, +, 3, -, 2]
 let arr2 = [] // [DO ALL X]
+let arr3 = [] // [DO ALL /]
+let arr4 = [] // [DO ALL +]
 
 arr0.forEach((e, i) => { // => arr1
   if (arr1.length === 0) arr1.push(e)
@@ -9,11 +11,21 @@ arr0.forEach((e, i) => { // => arr1
   else arr1.push(e)
 })
 
-arr1.forEach((e, i) => { // => arr2
-  if (i !== 0 && e === "X" && !isNaN(arr1[i - 1]) && !isNaN(arr1[i + 1])) arr2.push(arr1[i - 1] * arr1[i + 1])
-  else if (i !== 0 && arr1[i - 1] !== "X" && i !== 0 && arr1[i + 1] !== "X") arr2.push(e)
+arr1.forEach((e, i) => { // => arr2 // [DO ALL X]
+  if (e === "X" && !isNaN(arr1[i - 1]) && !isNaN(arr1[i + 1])) arr2.push(arr1[i - 1] * arr1[i + 1])
+  else if (arr1[i - 1] !== "X" && arr1[i + 1] !== "X") arr2.push(e)
+})
+
+arr2.forEach((e, i) => { // => arr3 // [DO ALL /]
+  if (e === "/" && !isNaN(arr2[i - 1]) && !isNaN(arr2[i + 1])) arr3.push(arr2[i - 1] / arr2[i + 1])
+  else if (arr2[i - 1] !== "/" && arr2[i + 1] !== "/") arr3.push(e)
+})
+
+arr3.forEach((e, i) => { // => arr4 // [DO ALL +]
+  if (e === "+" && !isNaN(arr3[i - 1]) && !isNaN(arr3[i + 1])) arr4.push(arr3[i - 1] + arr3[i + 1])
+  else if (arr3[i - 1] !== "+" && arr3[i + 1] !== "+") arr4.push(e)
 })
 
 console.log(
-  arr2
+  arr4
 )
