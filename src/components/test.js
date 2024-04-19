bbeb9dd1d361382c96e1a1a4a4215d747cbcc890
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-04-19 17:06:52 -0300

    code refactor

--- src/components/test.js
+++ src/components/test.js
@@ -1,4 +1,4 @@
-let init = "(((10+2x3x2)+2x1)x2)))".split("") // ['S'P'L'I'T'E'D'] // TEST TARGET
+let init = "((10x2x3/2+2)+2x1)x2".split("") // ['S'P'L'I'T'E'D'] // TEST TARGET
 let parsed = [] // [10, x, 30, +, 3, -, 2] // WELL PARSED
 
 init.forEach((e, i) => { // => parsed
@@ -7,8 +7,10 @@ init.forEach((e, i) => { // => parsed
   else parsed.push(e)
 })
 
-let openPar = parsed.indexOf(")") // FIND FIRST OPEN ( PARENTHESIS, MOVING INDEX
+let openPar = parsed.indexOf(")") // FIND FIRST OPEN ( PARENTHESIS NEXT TO ), MOVING INDEX
 let closePar = openPar;// FIND FIRST CLOSED ) PARENTHESIS
+
+console.log(parsed.indexOf(")") + 1)
 
 do {
   if (parsed[openPar] === "(") break;
@@ -23,14 +25,6 @@ let foundMul;
 let foundDiv;
 let firstOp;
 
-function updateMultiAndDiv() {
-  foundMul = toDo.indexOf("x") // update x index
-  foundDiv = toDo.indexOf("/") // update / index
-  if (foundMul < foundDiv && foundMul > 0 || foundMul > 0 && foundDiv === -1) firstOp = "x"
-  if (foundDiv < foundMul && foundDiv > 0 || foundDiv > 0 && foundMul === -1) firstOp = "/"
-  if (foundMul === -1 && foundDiv === -1) firstOp = undefined;
-}
-
 let opOne = { // operation One // x or /
   'x': function(a, b) { return parseInt(a) * parseInt(b) },
   '/': function(a, b) { return parseInt(a) / parseInt(b) }
@@ -40,7 +34,18 @@ let foundPlus = toDo.indexOf("+")
 let foundMin = toDo.indexOf("-")
 let secOp;
 
-function updatePlusAndMin() {
+let opTwo = { // operation Two // + or -
+  '+': function(a, b) { return parseInt(a) + parseInt(b) },
+  '-': function(a, b) { return parseInt(a) - parseInt(b) }
+};
+
+function updateVariables() { // multi, div, plus & minus
+  foundMul = toDo.indexOf("x") // update x index
+  foundDiv = toDo.indexOf("/") // update / index
+  if (foundMul < foundDiv && foundMul > 0 || foundMul > 0 && foundDiv === -1) firstOp = "x"
+  if (foundDiv < foundMul && foundDiv > 0 || foundDiv > 0 && foundMul === -1) firstOp = "/"
+  if (foundMul === -1 && foundDiv === -1) firstOp = undefined;
+
   foundPlus = toDo.indexOf("+") // update + index
   foundMin = toDo.indexOf("-") // update - index
   if (foundPlus < foundMin && foundPlus > 0 || foundPlus > 0 && foundMin === -1) secOp = "+" // update + or - found
@@ -48,20 +53,13 @@ function updatePlusAndMin() {
   if (foundPlus === -1 && foundMin === -1) secOp = undefined;
 }
 
-let opTwo = { // operation Two // + or -
-  '+': function(a, b) { return parseInt(a) + parseInt(b) },
-  '-': function(a, b) { return parseInt(a) - parseInt(b) }
-};
-
-updateMultiAndDiv()
-updatePlusAndMin()
+updateVariables()
 
 let curr;
 let idx = 1
 
 while (toDo.length !== 1 && firstOp !== undefined || secOp !== undefined) {
-  updateMultiAndDiv()
-  updatePlusAndMin()
+  updateVariables() // multi, div, plus & minus
 
   if (toDo[idx - 1] !== undefined && !isNaN(toDo[idx - 1]) && toDo[idx] === firstOp && !isNaN(toDo[idx + 1])) { // do al x or /
     curr = opOne[firstOp](toDo[idx - 1], toDo[idx + 1])
@@ -81,6 +79,8 @@ while (toDo.length !== 1 && firstOp !== undefined || secOp !== undefined) {
 console.log(
   //foundMul,
   //foundDiv,
+  `openPar: ${openPar}`,
+  `closePar: ${closePar}`,
   `firstOp: ${firstOp}`,
   `secOp: ${secOp}`,
   `toDo: ${toDo}`,
