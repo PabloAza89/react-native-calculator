a93aff4e3fe329f3d8dafbbc2c59621874628c20
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-04-19 13:29:37 -0300

    basic multi and div done

--- src/components/test.js
+++ src/components/test.js
@@ -1,4 +1,4 @@
-let init = "(((4/2x3)+2x1)x2)))".split("") // ['S'P'L'I'T'E'D'] // TEST TARGET
+let init = "(((10/2x3)+2x1)x2)))".split("") // ['S'P'L'I'T'E'D'] // TEST TARGET
 //let init = "4+1".split("") // ['S'P'L'I'T'E'D']
 // (2x2x2)
 // (2/(2x2x2))
@@ -34,49 +34,88 @@ toDo.splice(-1, 1) // Delete )
 
 
 
+
+
+
+
 let foundMul = toDo.indexOf("x")
 let foundDiv = toDo.indexOf("/")
 let firstOp; // firstOperator Found = x or /
-// firstOp = foundDiv
 if (foundMul < foundDiv && foundMul > 0 || foundMul > 0 && foundDiv === -1) firstOp = "x"
 if (foundDiv < foundMul && foundDiv > 0 || foundDiv > 0 && foundMul === -1) firstOp = "/"
-//if (foundDiv < foundMul && foundDiv > 0) firstOp = foundDiv
 
-//toDo = [ '4', 'x', '2', '+', '3' ]
-let curr = []
-let idx = 0
-let op = 0 // op = operator // 0 = x // 1 = /
-
-let opp = { // operation
+let opOne = { // operation One
   'x': function(a, b) { return parseInt(a) * parseInt(b) },
   '/': function(a, b) { return parseInt(a) / parseInt(b) }
 };
 
-if (firstOp !== undefined) {
-  do {
-    if (op === 0 && toDo[idx - 1] !== undefined && !isNaN(toDo[idx - 1]) && toDo[idx] === firstOp && !isNaN(toDo[idx + 1])) {
-      console.log("TT", idx, curr.length)
-      if (curr.length === 0) curr[0] = opp[firstOp](toDo[idx - 1], toDo[idx + 1])
-      else curr[0] = opp[firstOp](curr[0], toDo[idx + 1])
+let curr;
+let idx = 0
+let lap = 0 // op = operator // 0 = x // 1 = /
 
-      if (toDo[idx + 2] !== undefined ) idx = idx + 2
-      else idx++
+// toDo = [ '4', 'x', '2', '+', '3' ]
+
+if (firstOp !== undefined) {
+  
+  while (/* toDo.length !== 0 */ /* && */ lap < 2) {
+
+    foundMul = toDo.indexOf("x")
+    foundDiv = toDo.indexOf("/")
+    firstOp; // firstOperator Found = x or /
+    if (foundMul < foundDiv && foundMul > 0 || foundMul > 0 && foundDiv === -1) firstOp = "x"
+    if (foundDiv < foundMul && foundDiv > 0 || foundDiv > 0 && foundMul === -1) firstOp = "/"
+
+    foundMul = toDo.indexOf("x")
+    foundDiv = toDo.indexOf("/")
+    console.log(toDo)
+    console.log(firstOp)
+    console.log(foundMul)
+    console.log(foundDiv)
+
+
+    if (toDo[idx - 1] !== undefined && !isNaN(toDo[idx - 1]) && toDo[idx] === firstOp && !isNaN(toDo[idx + 1])) {
+      console.log("TT", idx/* , curr.length */)
+      //if (curr.length === 0) {
+        console.log("idx", idx)
+        curr = opOne[firstOp](toDo[idx - 1], toDo[idx + 1])
+        console.log("idx - 1", idx - 1)
+        console.log("idx - 1", idx + 1)
+
+        toDo.splice(idx - 1, 3)
+        toDo.splice(idx - 1, 0, curr)
+        console.log("toDo", toDo)
+
+        console.log("curr", curr)
+        console.log("toDo", toDo)
+        idx = 0
+        lap++
+        //break;
+      //}
+      // else {
+      //   curr[0] = opOne[firstOp](curr[0], toDo[idx + 1])
+      // }
+
+      // if (toDo[idx + 2] !== undefined ) idx = idx + 2
+      //else idx++
     }
     else idx++
-    if (idx === toDo.length - 1) {
-      op++;
-      idx = 0
-    }
-  } while (idx < toDo.length - 1 && op < 4)
+    if (idx === toDo.length - 1) lap++
+   // console.log("LAP", lap, "IDX", idx)
+    // if (idx === toDo.length - 1) {
+    //   //lap++;
+    //   lap++
+    //   idx = 0
+    //   console.log("LAP", lap, "IDX", idx)
+    // }
+  }
 }
 
 console.log(
   //foundMul,
   //foundDiv,
-  //firstOp,
-  //toDo
-  //firstOp
-  curr
+  `firstOp: ${firstOp}`,
+  `toDo: ${toDo}`,
+  `curr: ${curr}`,
 )
 
 
