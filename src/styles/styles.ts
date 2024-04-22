52a137e9f21337180c50fcaed6aa7c759e29a3ed
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-04-22 16:41:33 -0300

    editing calc function

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
-const opw = Dimensions.get('window').width / 100; // onePercentWidth
+const opw = Dimensions.get('window').width / 100; // onePercentWidth = 1%vw
 
 export const s = StyleSheet.create({
   background: {
