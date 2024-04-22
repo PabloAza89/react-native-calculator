399da35890809880c088e615e45db359cb8a314a
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-04-22 12:56:36 -0300

    editing user visual input

--- App.tsx
+++ App.tsx
@@ -20,22 +20,22 @@ const [ input, setInput ] = useState("");
 const [ resPressed, setResPressed ] = useState(false);
 
 //let input = "3-100"
-let arr0: any[] = input.split("") // ['S'P'L'I'T'E'D']
-let arr1: any[] = [] // [10, x, 33, +, 3, -, 2] // WELL PARSED
-let arr2: any[] = [] // [DO ALL x]
-let arr3: any[] = [] // [DO ALL /]
-let arr4: any[] = [] // [DO ALL +]
-let arr5: any[] = [] // [DO ALL -] // FINAL RES
+//let init: any[] = input.split("") // ['S'P'L'I'T'E'D']
+// let arr1: any[] = [] // [10, x, 33, +, 3, -, 2] // WELL PARSED
+// let arr2: any[] = [] // [DO ALL x]
+// let arr3: any[] = [] // [DO ALL /]
+// let arr4: any[] = [] // [DO ALL +]
+// let arr5: any[] = [] // [DO ALL -] // FINAL RES
 
-arr0.forEach((e, i) => { // => arr1
-  if (arr1.length === 0) arr1.push(e)
-  else if (!isNaN(arr0[i - 1]) && !isNaN(e)) arr1[arr1.length - 1] = arr1[arr1.length - 1].concat(e)
-  else arr1.push(e)
-})
+// arr0.forEach((e, i) => { // => arr1
+//   if (arr1.length === 0) arr1.push(e)
+//   else if (!isNaN(arr0[i - 1]) && !isNaN(e)) arr1[arr1.length - 1] = arr1[arr1.length - 1].concat(e)
+//   else arr1.push(e)
+// })
 
 
 
-  console.log("arr1:", arr1)
+  //console.log("arr1:", arr1)
   //console.log("arr5:", arr5)
 
 
@@ -44,7 +44,7 @@ arr0.forEach((e, i) => { // => arr1
       <View style={[s.contour]}>
         {/* <Text style={[s.result]}>{ input }</Text> */}
         {/* <Text style={[s.result]}>{ resPressed ? arr5 : arr1 }</Text> */}
-        <Text style={[s.result]}>{ arr0 }</Text>
+        <Text style={[s.result]}>{ input }</Text>
         <OwnButton input={input} setInput={setInput} value="(" smaller={true} />
         <OwnButton input={input} setInput={setInput} value=")" smaller={true} />
         <OwnButton input={input} setInput={setInput} value="C" smaller={true} setResPressed={setResPressed} />
@@ -65,7 +65,7 @@ arr0.forEach((e, i) => { // => arr1
         <OwnButton input={input} setInput={setInput} value="/" />
         <OwnButton input={input} setInput={setInput} value="0" />
         <OwnButton input={input} setInput={setInput} value="." />
-        <OwnButton input={input} setInput={setInput} value="=" setResPressed={setResPressed} arr5={arr5}  />
+        <OwnButton input={input} setInput={setInput} value="=" setResPressed={setResPressed} /* arr5={arr5} */ />
       </View>
     </View>
   );
--- src/components/OwnButton.tsx
+++ src/components/OwnButton.tsx
@@ -21,38 +21,112 @@ interface OwnButtonI {
 export function OwnButton({ value, input, setInput, setResPressed, resPressed, arr1, arr5, smaller }: OwnButtonI): React.JSX.Element {
   //console.log("PROPS", props)
   function handlePress() {
-
+    console.log("INPUT:", input)
+    console.log("INPUT length:", input.length)
     // console.log("resPressed", resPressed)
     // console.log("init", init)
     // console.log("val", val)
-    if (resPressed && input !== "" && value === "+") {
-      setResPressed(false);
-      setInput(arr1[0].toString());
-      console.log("EXECUTED")
-    }
 
-    if (value === "X") value = "x"
+    // test ↓↓↓
+    setInput((value * -1).toString()) // test
 
-    if (value === "C") { setResPressed(false); setInput("") }
-    else if (input === "" && !isNaN(value) || input !== "") {
-      if (value === "=") {
-        setResPressed(true);
-        setInput(arr5[0].toString())
-      }
-      //else if (value === "C") setInit("")
-      else if (value === "B") {
+    if (value === "C") { setInput(""); return } // CLEAR ALL
+
+    if (value === "B") { // Backspace
+      
+      if (input.slice(-3) === " x " || // if last input is an operator: "123 + "
+        input.slice(-3) === " / " ||
+        input.slice(-3) === " + " ||
+        input.slice(-3) === " - ") {
+        setInput(input.slice(0,-3))
+        return
+      } else { // else
         let seqOne = input.split("")
         seqOne.pop()
         let seqTwo = seqOne.join("")
         setInput(seqTwo)
+        return
       }
-      else setInput((prev: any) => prev + value)
+      
     }
+
+    if (
+      (input.slice(-3) === " x " ||
+      input.slice(-3) === " / " ||
+      input.slice(-3) === " + " ||
+      input.slice(-3) === " - " ||
+      input.slice(-1) === "(") &&
+      (value === "X" ||
+      value === "/" ||
+      value === "+" ||
+      value === "-" ||
+      value === ")") // STOP IF ATTEMP + AND + (REPEATED OPERATORS)
+    ) return
+
+    if (input.length === 0) {
+      if (
+        value === "/" ||
+        value === "." ||
+        value === "+" ||
+        value === "-" ||
+        value === "X" ||
+        value === ")"
+      ) return // STOP IF ) FIRST
+    }
+
+    if (
+      input.slice(-1) === ")" &&
+      value === "("
+    ) return // STOP IF ATTEMP )(
+
+    if (
+      input.slice(-1) === ")" &&
+      !isNaN(parseInt(value))
+    ) return // STOP IF ATTEMP )9
+
+    if (
+      !isNaN(parseInt(input.slice(-1))) &&
+      value === "("
+    ) return // STOP IF ATTEMP 9(
+
+    
+    if (value === "X") setInput((prev: any) => prev + " x ")
+    else if (value === "/") setInput((prev: any) => prev + " / ")
+    else if (value === "+") setInput((prev: any) => prev + " + ")
+    else if (value === "-") setInput((prev: any) => prev + " - ")
+    else setInput((prev: any) => prev + value)
+      //if (value === ")") return
+    //   setInput((prev: any) => prev + value)
+    // } 
+   //setInput((prev: any) => prev + value)
+
+    // if (resPressed && input !== "" && value === "+") {
+    //   setResPressed(false);
+    //   setInput(arr1[0].toString());
+    //   console.log("EXECUTED")
+    // }
+
+    // if (value === "X") value = "x"
+
+    // if (value === "C") { setResPressed(false); setInput("") }
+    // else if (input === "" && !isNaN(value) || input !== "") {
+    //   if (value === "=") {
+    //     setResPressed(true);
+    //     setInput(arr5[0].toString())
+    //   }
+    //   //else if (value === "C") setInit("")
+    //   else if (value === "B") {
+    //     let seqOne = input.split("")
+    //     seqOne.pop()
+    //     let seqTwo = seqOne.join("")
+    //     setInput(seqTwo)
+    //   }
+    //   else setInput((prev: any) => prev + value)
+    // }
   }
 
 
   return (
-
     <Text
       onPress={() => handlePress()}
       style={[ smaller ? s.ownButtonSmaller : s.ownButton ]}
