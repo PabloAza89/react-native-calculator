d2f2b7d4c787ad697e089d949d22ec8ee84a003b
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-05-31 19:52:19 -0300

    using react-native 0.73

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/Home/Home';
+import About from './src/components/About/About';
+import KnowMore from './src/components/KnowMore/KnowMore';
+
+const Stack: any = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          navigationBarHidden: true,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ animation: 'slide_from_right' }}
+        />
+        <Stack.Screen
+          name="About"
+          component={About}
+          options={{ animation: 'slide_from_right' }}
+        />
+        <Stack.Screen
+          name="KnowMore"
+          component={KnowMore}
+          options={{ animation: 'slide_from_right' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
