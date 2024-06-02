0cdc0e60ecb4356f236f06c9ab19dd4a69c51cfa
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-06-02 12:26:55 -0300

    project broken. Backup point

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/components/Home/Home';
 import About from './src/components/About/About';
 import KnowMore from './src/components/KnowMore/KnowMore';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+//import * as Font from 'expo-font';
 
 import {
   SafeAreaView,
@@ -17,38 +20,54 @@ import {
 
 const Stack: any = createNativeStackNavigator();
 
+SplashScreen.preventAutoHideAsync();
+
 function App(): React.JSX.Element {
 
-  return (
+  const [appIsReady, setAppIsReady] = useState(false);
 
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-          navigationBarColor: 'transparent', // BOTTOM
-          //navigationBarHidden: true, // BOTTOM
-        }}
-      >
-        {/* <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ animation: 'slide_from_right' }}
-        />
-        <Stack.Screen
-          name="About"
-          component={About}
-          options={{ animation: 'slide_from_right' }}
-        /> */}
-        <Stack.Screen
-          name="KnowMore"
-          component={KnowMore}
-          options={{ animation: 'slide_from_right' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+  useEffect(() => {
+    async function prepare() {
+      // Keep the splash screen visible
+      await SplashScreen.preventAutoHideAsync();
+      // Do what you need before the splash screen gets hidden
+      console.log("I'm a task that gets executed before splash screen disappears");
+      // Then tell the application to render
+      setAppIsReady(true);
+    }
+    prepare();
+  }, []);
 
-  );
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // Hide the splash screen
+      await SplashScreen.hideAsync();
+    }
+
+    
+
+  }, [appIsReady]);
+
+  if (appIsReady) {
+    return (
+      <View
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        onLayout={onLayoutRootView}>
+        <Text>AAAAAAAAAAAAAA! 👋</Text>
+        {/* <Entypo name="rocket" size={30} /> */}
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        onLayout={onLayoutRootView}>
+        <Text>BBBBBBBBBBBBBBBBBB! 👋</Text>
+        {/* <Entypo name="rocket" size={30} /> */}
+      </View>
+    );
+  }
+  
 }
 
 export default App;
