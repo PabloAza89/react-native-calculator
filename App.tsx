5f9e9fe77694a71702effcbc96163e64aa8165f8
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-07-18 12:13:27 -0300

    code cleaning

--- App.tsx
+++ App.tsx
@@ -1,58 +1,26 @@
-import 'react-native-gesture-handler';
-import React, { useEffect, useRef, useState } from "react";
-import { useNavigationState, CommonActions, NavigationContainer, useRoute, useNavigationContainerRef, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import { CommonActions, NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/components/Home/Home';
 import About from './src/components/About/About';
 import KnowMore from './src/components/KnowMore/KnowMore';
 import BootSplash from "react-native-bootsplash";
-//import { Dimensions, AppState } from 'react-native';
 import * as Font from 'expo-font';
-//import { Font } from 'expo';
-//import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
-import { opw, /* ins, */ /* dH, wH, */ aB/* , nB */ } from './src/components/constants';
-
-import { SafeAreaInsetsContext, useSafeAreaInsets, SafeAreaProvider  } from 'react-native-safe-area-context';
-//let ins = useSafeAreaInsets(); // insets
-//import { SafeAreaView } from 'react-native-safe-area-context';
-
-import {
-  Image,
-  AppState,
-  Dimensions,
-  SafeAreaView
-} from 'react-native';
+import { Image, AppState, Dimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import FastImage from 'react-native-fast-image'
-//import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import { AntDesign, Entypo, FontAwesome5, Ionicons, MaterialIcons, SimpleLineIcons } from '@expo/vector-icons';
-import { s } from './src/components/KnowMore/KnowMoreCSS';
-//import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Stack: any = createNativeStackNavigator();
 
-export const Screens =
-  <Stack.Screen
-    name="About"
-    component={ About }
-    options={{
-      animationEnabled: false,
-      //animation: 'slide_from_right'
-      animation: 'none'
-    }}
-  />
-
-export const NavigatorMapper = (testRef: any, screens: any[]) => {
+export const NavigatorMapper = (animation: any, screens: any[]) => {
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
         gestureEnabled: false,
         navigationBarColor: 'rgba(0, 0, 0, 0.2)',
-        //animation: 'slide_from_right',
-        //animation: 'slide_from_right',
-        animation: testRef
-        //animation: 'none'
+        animation: animation
       }}
     >
       { screens.map((e: any) => e) }
@@ -64,57 +32,7 @@ function App(): React.JSX.Element {
 
   const navigationRef: any = useNavigationContainerRef();
 
-  // useEffect(() => {
-  //   const init = async () => {
-  //     // …do multiple sync or async tasks
-  //   };
-
-  //   init().finally(async () => {
-  //     await RNBootSplash.hide({ fade: true });
-  //     console.log("Bootsplash has been hidden successfully");
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   RNBootSplash.hide({ fade: true })
-  //   //RNBootSplash.hide()
-  // }, [])
-
-  // <NavigationContainer onReady={() => RNBootSplash.hide({ fade: true })}>
-  // different === navigationBar Enabled
-
-  //let res = await readData("savedRoute") // RESPONSE ROUTE
-
-  //const [ routeGo, setRouteGo ] = useState('KnowMore')
-  //let routeGo = useRef('KnowMore')
-  let routeGo = useRef('Home')
-  //routeGo.current = 'KnowMore'
-  const [ finished, setFinished ] = useState(false)
-  //let resRoute = useRef<any>("Home")
-
-  //const testRef = useNavigationContainerRef();
-  //const testRef = useRef('slide_from_right');
-  //const testRef = useRef('none');
-  const [ testRef, setTestRef ] = useState('none');
-  //const [ testRef, setTestRef ] = useState('slide_from_right');
-
-  // const [ initialState, setInitialState ] = useState({
-  //   index: 0, // to load the second screen which is LastNameView
-  //   routes: [
-  //     { name: 'Home' },
-  //     /* { name: 'About' }, */
-  //     //{ name: 'KnowMore' },
-  //   ],
-  // })
-
-  let initialState = {
-    index: 0, // to load the second screen which is LastNameView
-    routes: [
-      { name: 'Home' },
-      /* { name: 'About' }, */
-      //{ name: 'KnowMore' },
-    ],
-  }
+  const [ animation, setAnimation ] = useState('none'); // NO SCREEN ANIMATION
 
   let routes = [
     { index: 2, routes: [{ name: 'Home' }, { name: 'About' }, { name: 'KnowMore' }] },
@@ -133,8 +51,6 @@ function App(): React.JSX.Element {
 
       if (resInput !== undefined && resInput !== null) setInput(resInput)
       if (resSecInput !== undefined && resSecInput !== null) setSecInput(resSecInput)
- 
-
 
       await Font.loadAsync({
         ...AntDesign.font,
@@ -142,8 +58,7 @@ function App(): React.JSX.Element {
         ...FontAwesome5.font,
         ...Ionicons.font,
         ...MaterialIcons.font,
-        ...SimpleLineIcons.font,
-        //'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        ...SimpleLineIcons.font
       })
 
       async function qq() {
@@ -156,35 +71,19 @@ function App(): React.JSX.Element {
             Date.now() - parseInt(resDate) < 60000 &&
             resHeight !== Dimensions.get('window').height.toString()
           ) {
-            console.log("WINDOWS HAS CHANGED !")
-            //setTestRef('none')
             resRoute === "KnowMore" ? navigationRef.current?.dispatch(CommonActions.reset(routes[0])) :
             resRoute === "About" ? navigationRef.current?.dispatch(CommonActions.reset(routes[1])) :
             navigationRef.current?.dispatch(CommonActions.reset(routes[2]))
           } // else console.log("WINDOWS NOT HAS CHANGED.")
         }
-
       }
-
       await qq()
-      
-      
       .then(() => {
-
-        
-
-        //setTimeout(() => BootSplash.hide(), 200) // AVOID ICON BLINKING
-        setTimeout(() => {
-          //                                                             la primera vez y
-          //console.log("ENTRO ACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") // cuando cambia el height
-          setTestRef('slide_from_right')
+        setTimeout(() => { // ONLY FIRST TIME & WHEN DEVICE WINDOW HEIGHT CHANGES
+          setAnimation('slide_from_right') // SLIDE SCREEN ANIMATION
           BootSplash.hide()
-          
         }, 200) // AVOID ICON BLINKING
       })
-      //.then(() => { setTestRef('slide_from_right') })
-      //.then(() => { testRef.current = 'slide_from_right' })
-      //.then(() => { setTestRef('slide_from_right') })
     }
     allPreloads()
   }, []);
@@ -198,15 +97,9 @@ function App(): React.JSX.Element {
       saveData("savedSecInput", secInput)
       saveData("savedDate", Date.now().toString())
       saveData("savedHeight", Dimensions.get('window').height.toString())
-      //console.log("APP CURRENT HEIGHT", Dimensions.get('window').height.toString())
-      //console.log("savedInput", input)
-      //console.log("savedSecInput", secInput)
 
       let array = navigationRef.current?.getState().routes // INSIDE ANY COMPONENT: navigation.getState().routes
-      if (array !== undefined) {
-        //console.log("BLUR SAVE ROUTE", array[array.length - 1].name)
-        saveData("savedRoute", array[array.length - 1].name) // SAVE LAST ROUTE ON APP BLUR
-      }
+      if (array !== undefined) saveData("savedRoute", array[array.length - 1].name) // SAVE LAST ROUTE ON APP BLUR
     })
     return () => blur.remove();
   }, [input, secInput]);
@@ -216,7 +109,6 @@ function App(): React.JSX.Element {
     catch(e) { }
   };
 
-
   const readData = async (key: string) => {
     try { return await AsyncStorage.getItem(key) }
     catch(e) { }
@@ -225,12 +117,9 @@ function App(): React.JSX.Element {
   FastImage.preload([{ uri: Image.resolveAssetSource(require('./src/images/profile.png')).uri }])
 
   return (
-    <NavigationContainer
-      ref={navigationRef}
-      //initialState={initialState.current}
-    >
+    <NavigationContainer ref={navigationRef}>
       {
-        NavigatorMapper(testRef,
+        NavigatorMapper(animation,
           [<Stack.Screen
             name="Home"
             key={"Home"}
@@ -247,24 +136,12 @@ function App(): React.JSX.Element {
             name="About"
             key={"About"}
             component={ About }
-            // options={{
-            //   animationEnabled: false,
-            //   //animation: 'slide_from_right'
-            //   animation: 'none'
-            // }}
           />,
           <Stack.Screen
             name="KnowMore"
             key={"KnowMore"}
-            //component={ KnowMore }
-          >
-            {
-              (props: any) =>
-              <KnowMore
-                {...props} //setTestRef={setTestRef}
-              />
-            }
-          </Stack.Screen>]
+            component={ KnowMore }
+          />]
 
         )
       }
