56c6f9ede016109934052856528eff47f440fa9f
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2025-09-03 15:23:07 -0300

    code cleaning & refactor

--- android/app/src/main/java/com/reactnativecalculator/MainActivity.kt
+++ android/app/src/main/java/com/reactnativecalculator/MainActivity.kt
@@ -2,17 +2,35 @@ package com.reactnativecalculator
 
 import android.content.Context
 import android.content.Context.SENSOR_SERVICE
+import android.content.res.Configuration
+import android.graphics.Insets
+import android.graphics.Rect
+import android.hardware.display.DisplayManager
 import android.hardware.SensorManager
 import android.hardware.Sensor
 import android.hardware.SensorEventListener
 import android.hardware.SensorEvent
 import android.os.Bundle
+import android.os.Build
 import android.provider.Settings
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.WindowManager
+import android.view.Display
+import android.view.OrientationEventListener
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.WindowInsets
 
+import androidx.annotation.RequiresApi
+import androidx.core.util.Consumer
+import androidx.core.view.WindowCompat
+import androidx.window.java.layout.WindowInfoTrackerCallbackAdapter
 import androidx.window.layout.WindowInfoTracker
 import androidx.window.layout.WindowMetricsCalculator
+import androidx.window.layout.WindowLayoutInfo
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.DisplayFeature
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -25,155 +43,57 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.LifecycleEventListener
 import com.facebook.react.bridge.ReactContext
-//import com.facebook.react.ReactContext
 import com.facebook.react.modules.core.DeviceEventManagerModule
 
 import com.reactnativecalculator.hingeBoundsClass
-//import com.reactnativecalculator.currentInsetsClass
+import com.reactnativecalculator.R
 
 import com.zoontek.rnbootsplash.RNBootSplash
 
 import expo.modules.ReactActivityDelegateWrapper
 
+import java.util.concurrent.Executors
+
+import kotlin.math.min
+import kotlin.properties.Delegates
 import kotlin.reflect.full.declaredMemberProperties
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-
-// TEST //
-
-import androidx.window.layout.WindowLayoutInfo
-import androidx.window.layout.FoldingFeature
-import android.hardware.display.DisplayManager
-import android.view.Display
-import android.util.DisplayMetrics
-import androidx.window.layout.DisplayFeature
-//import androidx.window.layout.WindowLayoutComponent
-//import androidx.window.extensions.WindowExtensions
-//import androidx.window.extensions.layout.WindowLayoutComponent
-//import androidx.window.extensions.layout.WindowLayoutComponent
-
-//import android.util.DisplayMetrics
-
-//import android.content.Context
-//import android.hardware.SensorManager
-import android.view.OrientationEventListener
-
-import android.content.res.Configuration
-
-import kotlinx.coroutines.*
-
-import androidx.window.java.layout.WindowInfoTrackerCallbackAdapter
-import java.util.concurrent.Executors
-import androidx.core.util.Consumer
-
-//import androidx.window.common.CommonFoldingFeature
-
-import android.os.Build
-import android.view.View
-import androidx.annotation.RequiresApi
-import android.view.WindowInsets
-
-//import android.os.Build
-import android.view.ViewTreeObserver
-import android.widget.LinearLayout
-import com.reactnativecalculator.R
-
-//import java.awt.Insets
-import android.graphics.Insets
-import android.graphics.Rect
-import kotlin.properties.Delegates
-
-import androidx.core.view.WindowCompat
-
-import com.facebook.react.bridge.ReactApplicationContext
-
-
-import com.facebook.react.ReactPackage
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.uimanager.ViewManager
-import com.facebook.react.uimanager.ReactShadowNode
-import com.facebook.react.bridge.NativeModule
-
-import com.facebook.react.bridge.WritableMap
-
-//import android.os.Handler
-import java.util.Timer
-import kotlin.concurrent.schedule
-
-import com.facebook.react.uimanager.util.ReactFindViewUtil
-import android.graphics.Color;
-import kotlin.math.min
-
-//import android.R
-
-// TEST //
+import kotlinx.coroutines.Job
+//import kotlinx.coroutines.*
 
 @Suppress("DEPRECATION")
-class MainActivity : ReactActivity(), ReactInstanceEventListener {
-
-  
+class MainActivity : ReactActivity(), ReactInstanceEventListener {  
   lateinit var currentOrientation: String// = "portrait" // DEFAULT
   var canUpdate: Boolean = true
   var sendUpdate: Boolean = false
-  //val dotsPerInch: Double = this@MainActivity.resources.displayMetrics.density.toDouble() // Float --> Double
-  //lateinit var dotsPerInch: Double
   var dotsPerInch: Double by Delegates.notNull<Double>()
-  //var count: Int by Delegates.notNull<Int>()
-
-  //val mainMap = Arguments.createMap()
-  //lateinit var windowMutableMap: MutableMap<String, Double>
-  //lateinit var windowMutableMap: MutableMap<String, Int>
-  //lateinit var windowMutableMap = mutableMapOf<String, Int>()
-  //val currentWindow = mutableMapOf<String, Int>()
   var currentWindow: MutableMap<String, Int> = mutableMapOf()
   var currentHingeBounds: MutableMap<String, Int> = mutableMapOf()
-  //val windowMap = Arguments.createMap()
   lateinit var currentState: String;
-  //lateinit var currentInsets: Insets
-  //lateinit var currentInsets: Insets
   lateinit var currentInsets: Rect
-  //val currentInsets = currentInsetsClass()
-
   lateinit var rootView: View
 
   lateinit var testVar: String
 
   override fun onCreate(savedInstanceState: Bundle?) {
-
-    // fun asd() {
-    //   Log.d("LOG", "TEST 111")
-    // }
-
-    // asd()
-    
     RNBootSplash.init(this, R.style.Start); // initialize the splash screen
     super.onCreate(null); // super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
     WindowCompat.setDecorFitsSystemWindows(window, false)
-
     dotsPerInch = this@MainActivity.resources.displayMetrics.density.toDouble() // Float --> Double
-
     rootView = findViewById<View>(android.R.id.content).rootView
-
-    
-
     rootView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-        override fun onGlobalLayout() {
-          if (canUpdate) updateUI(null, true)
-        }
+      override fun onGlobalLayout() {
+        if (canUpdate) updateUI(null, true)
+      }
     })
-
-
   }
-
 
   override fun onResume() {
     super.onResume()
     reactInstanceManager.addReactInstanceEventListener(this)
-
   }
 
   override fun onPause() {
@@ -182,8 +102,6 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
   }
 
   override fun onReactContextInitialized(context: ReactContext) {
-
-
     class ListenerCallback : Consumer<WindowLayoutInfo> {
       override fun accept(newLayoutInfo: WindowLayoutInfo) {
         if (canUpdate) updateUI(newLayoutInfo, false)
@@ -195,7 +113,6 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
     val windowInfoTracker = WindowInfoTrackerCallbackAdapter(WindowInfoTracker.getOrCreate(this@MainActivity))
 
     val lifecycleEventListener = object: LifecycleEventListener {
-
       override fun onHostResume() {
         windowInfoTracker.addWindowLayoutInfoListener(
           this@MainActivity,
@@ -215,20 +132,13 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
         //   rootView.viewTreeObserver.removeGlobalOnLayoutListener(layoutListener)
         //   Log.d("LOG", "REMOVE LAYOUT LISTENER OTHER");
         // }
-        
       }
       override fun onHostDestroy() { }
     }
     context.addLifecycleEventListener(lifecycleEventListener)
-
-
-
-
   }
 
-
   fun updateUI(incomingWindowLayoutInfo: WindowLayoutInfo?, manual: Boolean) {
-
     canUpdate = false // FLAG FOR updateUI()
 
     // BEGIN ORIENTATION //
@@ -237,13 +147,10 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
     else { currentOrientation = "landscape" }
     // END ORIENTATION //
 
-    
     lateinit var job: Job
 
     fun collectAndCancel(windowLayoutInfo: WindowLayoutInfo, doJob: Boolean) {
-
-      // windowLayoutInfo
-      Log.d("LOG", "RESPONSE ${windowLayoutInfo}");
+      Log.d("LOG", "RESPONSE ${windowLayoutInfo}"); // windowLayoutInfo
 
       if (doJob) job.cancel();
 
@@ -289,21 +196,11 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
 
       val foldingFeature = windowLayoutInfo.displayFeatures.filterIsInstance<FoldingFeature>().firstOrNull()
 
-      //var state: String = "portrait";
-      //lateinit var state: String;
-
-      //val hingeBounds = hingeBoundsClass()
-
       var newHingeBounds: MutableMap<String, Int> = mutableMapOf()
 
       lateinit var newState: String;
 
       if (foldingFeature != null) {
-        // hingeBounds.left = foldingFeature.bounds.left // / dotsPerInch
-        // hingeBounds.top = foldingFeature.bounds.top // / dotsPerInch
-        // hingeBounds.right = foldingFeature.bounds.right // / dotsPerInch
-        // hingeBounds.bottom = foldingFeature.bounds.bottom // / dotsPerInch
-
         newHingeBounds = mutableMapOf(
           "left" to foldingFeature.bounds.left,
           "top" to foldingFeature.bounds.top,
@@ -326,7 +223,6 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
 
 
       if (sendUpdate) {
-        //mainMap.putMap("hingeBounds", hingeBoundsMap);
         mainMap.putMap("hingeBounds", Arguments.createMap().apply {
           putDouble("left", currentHingeBounds["left"]!! / dotsPerInch)
           putDouble("top", currentHingeBounds["top"]!! / dotsPerInch)
@@ -334,12 +230,10 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
           putDouble("bottom", currentHingeBounds["bottom"]!! / dotsPerInch)
         });
         mainMap.putString("state", currentState); // flat, tabletop..
-        //mainMap.putMap("window", windowMap);
         mainMap.putMap("window", Arguments.createMap().apply {
           putDouble("width", currentWindow["width"]!! / dotsPerInch)
           putDouble("height", currentWindow["height"]!! / dotsPerInch)
         });
-        //mainMap.putMap("insets", insetsMap)
         mainMap.putMap("insets", Arguments.createMap().apply {
           putDouble("left", currentInsets.left / dotsPerInch)
           putDouble("top", currentInsets.top / dotsPerInch)
@@ -360,18 +254,13 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
                 ?.emit("LayoutInfo", mainMap)
             }
           })
-          //return null
         } else {
           reactInstanceManager.currentReactContext
             ?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
             ?.emit("LayoutInfo", mainMap)
-          //return null
         }
-
-
         sendUpdate = false // RESET UPDATE FLAG
       }
-
       canUpdate = true // FLAG FOR updateUI()
     }
 
@@ -383,9 +272,7 @@ class MainActivity : ReactActivity(), ReactInstanceEventListener {
           .collect { collectAndCancel(it, true) }
       };
     }
-
   }
-
 
   // Returns the name of the main component registered from JavaScript. This is used to schedule rendering of the component.
   override fun getMainComponentName(): String = "reactNativeCalculator"
