25ec4275cb007f96610ecd24afdf8a6db852a0ce
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2025-10-06 21:40:49 -0300

    successful new CustomScrollView test on previous KnowMore.tsx

--- src/components/KnowMore/CustomScrollView.tsx
+++ src/components/KnowMore/CustomScrollView.tsx
@@ -3,10 +3,10 @@ import { requireNativeComponent, View } from 'react-native';
 const CustomScrollViewComponent = (props: any) => {
   return (<CustomScrollView {...props} style={props.style} ref={props.scrollRef}>
     <View collapsable={false} style={{
-      paddingLeft: props.contentContainerStyle.paddingLeft | 0,
-      paddingTop: props.contentContainerStyle.paddingTop | 0,
-      paddingRight: props.contentContainerStyle.paddingRight | 0,
-      paddingBottom: props.contentContainerStyle.paddingBottom | 0,
+      paddingLeft: props?.contentContainerStyle?.paddingLeft | 0,
+      paddingTop: props?.contentContainerStyle?.paddingTop | 0,
+      paddingRight: props?.contentContainerStyle?.paddingRight | 0,
+      paddingBottom: props?.contentContainerStyle?.paddingBottom | 0,
     }}>
       {props.children}
     </View>
